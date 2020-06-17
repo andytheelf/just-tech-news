46c7ba4585c6045971c6065afc79f6b537b6e383
andytheelf <ballew22@gmail.com>
2020-06-17 17:27:04 -0500

    if statment additions

---
+++ public/javascript/upvote.js
@@ -0,0 +1,15 @@
+const response = await fetch('/api/posts/upvote', {
+    method: 'PUT',
+    body: JSON.stringify({
+        post_id: id
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+if (response.ok) {
+    document.location.reload();
+} else {
+    alert(response.statusText);
+}
